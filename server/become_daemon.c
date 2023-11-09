a9cc9a3df91ee0a8e24d7d80867b8341b9f95ae8
dmalves <tcleval@gmail.com>
2023-11-08 23:05:58 -0300

    -

--- server/become_daemon.c
+++ server/become_daemon.c
@@ -1,56 +1,69 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2023.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU Lesser General Public License as published   *
-* by the Free Software Foundation, either version 3 or (at your option)   *
-* any later version. This program is distributed without any warranty.    *
-* See the files COPYING.lgpl-v3 and COPYING.gpl-v3 for details.           *
-\*************************************************************************/
-
-/* become_daemon.c
-
-   A function encapsulating the steps in becoming a daemon.
-*/
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "become_daemon.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <syslog.h>
 
-int                                     /* Returns 0 on success, -1 on error */
-becomeDaemon()
+int				/* Returns 0 on success, -1 on error */
+becomeDaemon ()
 {
-    int maxfd, fd;
+  int maxfd, fd;
 
-    switch (fork()) {                   /* Become background process */
-    case -1: return -1;
-    case 0:  break;                     /* Child falls through... */
-    default: _exit(EXIT_SUCCESS);       /* while parent terminates */
+  switch (fork ())
+    {
+    case -1:
+      return -1;
+    case 0:
+      break;
+    default:
+      _exit (EXIT_SUCCESS);
+    }
+  umask (0);
+
+  if (setsid () == -1)
+    {
+      syslog (LOG_ERR, "setsid()");
+      return -1;
+    }
+  if (chdir ("/") == -1)
+    {
+      syslog (LOG_ERR, "can not change to dir /");
+      return -1;
     }
 
-    if (setsid() == -1)                 /* Become leader of new session */
-        return -1;
+  close (STDIN_FILENO);
+  close (STDOUT_FILENO);
+  close (STDERR_FILENO);
 
-    switch (fork()) {                   /* Ensure we are not session leader */
-    case -1: return -1;
-    case 0:  break;
-    default: _exit(EXIT_SUCCESS);
+
+  fd = open ("/dev/null", O_RDWR);
+
+  if (fd == -1)
+    {
+      syslog (LOG_ERR, "can not open /dev/null");
+      return -1;
     }
 
+  if (dup2 (fd, STDIN_FILENO) == -1)
+    {
+      syslog (LOG_ERR, "stdin redirect failed");
+      return -1;
+    }
 
-    
-	close(STDIN_FILENO);            /* Reopen standard fd's to /dev/null */
+  if (dup2 (fd, STDOUT_FILENO) == -1)
+    {
+      syslog (LOG_ERR, "stdout redirect failed");
+      return -1;
+    }
 
-	fd = open("/dev/null", O_RDWR);
+  if (dup2 (fd, STDERR_FILENO) == -1)
+    {
+      syslog (LOG_ERR, "stderr redirect failed");
+      return -1;
+    }
 
-	if (fd != STDIN_FILENO)         /* 'fd' should be 0 */
-		return -1;
-	if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
-		return -1;
-	if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
-		return -1;
-    
+  close (fd);
+  return 0;
 
-    return 0;
 }
